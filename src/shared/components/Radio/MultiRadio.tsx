f62ecdbe92537dc552f4dd50a097ca53c7704847
mung96 <mung5079@gmail.com>
2025-09-08 21:32:52 +0900

    fix: 다중 선택 안되던 버그 수정

--- src/shared/components/Radio/MultiRadio.tsx
+++ src/shared/components/Radio/MultiRadio.tsx
@@ -1,6 +1,8 @@
-import { useMultiSelect } from "@/shared/components/MultiSelect";
+import {
+  useMultiSelect,
+  MultiSelectItem,
+} from "@/shared/components/MultiSelect";
 import { RadioUI } from "@/shared/components/Radio/RadioUI";
-import { SingleSelectItem } from "@/shared/components/SingleSelect";
 
 type Props = {
   label: string;
@@ -11,13 +13,13 @@ export const MultiRadio = ({ label, value }: Props) => {
   const { getIsChecked } = useMultiSelect();
 
   return (
-    <SingleSelectItem
+    <MultiSelectItem
       aria-label={label}
       value={value}
       className="flex gap-2 items-center"
     >
       <RadioUI checked={getIsChecked(value)} />
       <p className="text-black body-inter">{label}</p>
-    </SingleSelectItem>
+    </MultiSelectItem>
   );
 };
