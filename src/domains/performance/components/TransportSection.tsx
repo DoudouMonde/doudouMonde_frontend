2c43bdfde6f7c469369191e97b7bf9845eee5fa6
mung96 <mung5079@gmail.com>
2025-09-13 00:00:40 +0900

    feat: kakao map

--- src/domains/performance/components/TransportSection.tsx
+++ src/domains/performance/components/TransportSection.tsx
@@ -1,15 +1,17 @@
 import { PerformanceDetail } from "@/domains/performance/types";
+import KakaoMap from "@/shared/components/KakaoMap";
 
 type Props = {};
 
 export const TransportSection = ({}: Props) => {
   return (
     <div className="p-4">
-      <iframe
+      {/* <iframe
         className="w-full h-full"
         src="https://map.kakao.com/?sName=서울역&eName=강남역"
         loading="lazy"
-      ></iframe>
+      ></iframe> */}
+      <KakaoMap width="85%" height="20dvh" lat={37.501389} lng={126.978} />
     </div>
   );
 };
---
+++ src/shared/components/KakaoMap/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+
+type Props = {
+  width: string;
+  height: string;
+  lat: number;
+  lng: number;
+};
+
+export default function KakaoMap({ width, height, lat, lng }: Props) {
+  return (
+    <Map center={{ lat, lng }} style={{ width, height, zIndex: -0 }}>
+      <MapMarker position={{ lat, lng }} />
+    </Map>
+  );
+}
