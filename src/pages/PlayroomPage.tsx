3ff4c3859c0319d145594dc7e42593ad4c0e7aac
mingeung <jmk2229@naver.com>
2025-10-25 12:30:58 +0900

    playroom 화면크기 잘리지 않게 수정

--- src/pages/PlayroomPage.tsx
+++ src/pages/PlayroomPage.tsx
@@ -6,19 +6,15 @@ export const PlayroomPage = () => {
   const navigate = useNavigate();
 
   const handleStart = () => {
-    console.log("놀이방 시작!");
-    // 공연선택 페이지로 이동
     navigate(PATH.SELECT_PERFORMANCE);
   };
 
   const handleSkip = () => {
-    console.log("다음에 하기");
-    // 리뷰 목록 페이지로 이동
     navigate("/playroom/reviews");
   };
 
   return (
-    <div className="w-full h-screen mb-18">
+    <div className="w-full h-full mb-18">
       <LandingPage onStart={handleStart} onSkip={handleSkip} />
     </div>
   );
