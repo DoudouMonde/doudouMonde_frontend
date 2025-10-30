d75d08553ea79d9cc0f7253f79945bef99253860
mingeung <jmk2229@naver.com>
2025-10-30 14:12:15 +0900

    feat: 아이등록 요청 스키마 생성

--- src/domains/child/schemas/ChildRequestSchema.ts
+++ src/domains/child/schemas/ChildRequestSchema.ts
@@ -1,4 +1,13 @@
-import {z} from "zod";
+import { z } from "zod";
+import { Gender, Profile } from "@/shared/types";
 
+const genderValues = Object.values(Gender) as [string, ...string[]];
+const profileValues = Object.values(Profile) as [string, ...string[]];
 
-import { Gender, {} } from "@/entities";
+export const ChildRequestSchema = z.object({
+  name: z.string().min(1, { message: "이름을 입력해주세요." }),
+  birthday: z.string().min(1, { message: "생년월일을 입력해주세요." }),
+
+  gender: z.enum(genderValues),
+  profile: z.enum(profileValues),
+});
