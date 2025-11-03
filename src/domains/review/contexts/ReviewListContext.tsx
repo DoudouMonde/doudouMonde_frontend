08bd1ad112aaf79cd34d07cb5779b53b2db27f60
mingeung <jmk2229@naver.com>
2025-11-03 15:22:35 +0900

    feat : ReviewListContext 작성

--- src/domains/review/contexts/ReviewListContext.tsx
+++ src/domains/review/contexts/ReviewListContext.tsx
@@ -1,1 +1,11 @@
-export const ReviewListContext = () => {};
+import { createContext, useContext } from "react";
+import { useReviewList } from "../hooks/useReviewLIst";
+
+export type ReviewListContextType = ReturnType<typeof useReviewList>;
+
+export const ReviewListContext = createContext<ReviewListContextType | null>(null);
+
+export const useReviewListContext = () => {
+  const ctx = useContext(ReviewListContext);
+  if (!ctx) throw new Error("ReviewLiseContext는 Provider 안에서만 사용 가능합니다. ");
+};
