e26d474272bfaadaaa495960c54f9ec4eadb78e0
mingeung <jmk2229@naver.com>
2025-11-03 15:12:31 +0900

    feat: 리뷰 랜딩 페이지 구조 그리기

--- src/pages/PlayroomPage.tsx
+++ src/pages/PlayroomPage.tsx
@@ -7,30 +7,41 @@ import { useChildNames } from "@/domains/playroom/features/landing/useChildNames
 import { useBookPopup } from "@/domains/playroom/features/landing/useBookPopup";
 
 export const PlayroomPage = () => {
-  const navigate = useNavigate();
+  const { reviewCount } = useReviewContext(); //만들 것임요
 
-  const reviewCount = useReviewCount();
-  const childNames = useChildNames();
-  const { open, closePopup } = useBookPopup(reviewCount, 9);
-
-  const handleStart = () => navigate(PATH.SELECT_PERFORMANCE);
-  const handleSkip = () => navigate("/playroom/reviews");
-  const handlePurchase = () => {
-    closePopup();
-    navigate(PATH.STORY_VILLAGE_BOOK);
-  };
-
-  return (
-    <div className="w-full h-full mb-18">
-      <LandingHero
-        reviewCount={reviewCount}
-        childNames={childNames}
-        onStart={handleStart}
-        onSkip={handleSkip}
-        popup={open}
-        onPurchase={handlePurchase}
-        onClosePopup={closePopup}
-      />
-    </div>
-  );
+  reviewCount == 0 ? <EmptyReviewsPage /> : <ReviewLandingPage />;
 };
+
+//기존 코드 LandingHero 페이지로 넘기는 과정
+// export const PlayroomPage = () => {
+//   const navigate = useNavigate();
+
+//   const reviewCount = useReviewCount();
+//   const childNames = useChildNames();
+//   const { open, closePopup } = useBookPopup(reviewCount, 9);
+
+//   const handleStart = () => navigate(PATH.SELECT_PERFORMANCE);
+//   const handleSkip = () => navigate("/playroom/reviews");
+//   const handlePurchase = () => {
+//     closePopup();
+//     navigate(PATH.STORY_VILLAGE_BOOK);
+//   };
+
+//   return (
+//     <div className="w-full h-full mb-18">
+//       <LandingHero
+//         reviewCount={reviewCount}
+//         childNames={childNames}
+//         onStart={handleStart}
+//         onSkip={handleSkip}
+//         popup={open}
+//         onPurchase={handlePurchase}
+//         onClosePopup={closePopup}
+//       />
+//     </div>
+//   );
+// };
+
+//PlayroomPage 페이지
+
+//-playromm Lading page - 리뷰 0개일 때
